b70f912f8e8f7da0fbe5967970aa738b57fea6e3
nsaito-gmo-ins <nsaito-gmo-ins>
2018-02-20 09:13:10 +0900

    add itp_1_4_b

--- itp_1_4_b.c
+++ itp_1_4_b.c
@@ -1,25 +1,16 @@
 /*
- * input: a op b
- * output: for each dataset, print the value line
+ * input: r(real)
+ * output: Print the area and circumference of the circle in a line. Put a single space between them. The output should not contain an absolute error greater than 10-5.
  */
 #include <stdio.h>
+#include <stdlib.h>
+
+#define PI 3.1415926535897932
 
 int main()
 {
-    int i,a,b;
-    char op;
-    for (i=1;i>0;i++){
-        scanf("%d %c %d",&a,&op,&b);
-        if ( op == '?')
-            break;
-        else if ( op == '+')
-            printf("%d\n",a+b);
-        else if ( op == '-')
-            printf("%d\n",a-b);
-        else if ( op == '*')
-            printf("%d\n",a*b);
-        else if ( op == '/')
-            printf("%d\n",a/b);
-    }
+    double r;
+    scanf("%lf",&r);
+    printf("%lf %lf\n", PI*r*r, 2*PI*r);
     return 0;
 }
---
+++ itp_1_4_b.php
@@ -0,0 +1,4 @@
+<?php
+
+fscanf(STDIN, '%f', $r);
+printf('%f %f%s', $r*$r*M_PI, 2*$r*M_PI,PHP_EOL);
