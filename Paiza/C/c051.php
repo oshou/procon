608931c9e966451c492521c7ae269f98e999bd93
oshou <adf1985adf@gmail.com>
2019-12-19 05:57:18 +0900

    post

---
+++ Paiza/C/c051.php
@@ -0,0 +1,14 @@
+<?php
+#https://www.softel.co.jp/blogs/tech/archives/2263
+# 順列 permutation
+# 組み合わせ combination
+
+function combination($n, $i, $s)
+{
+    if ($n === 0) {
+        return;
+    }
+    for ($j = 0; $j < 4; $j++) { }
+};
+
+$arr = fscanf(STDIN, "%d %d %d %d");
